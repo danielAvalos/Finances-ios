2396d18b9dae2ea5b9fcc6228e8e590ea4762e6e
daniel.avalos <jdanielavalosa@gmail.com>
2021-01-21 18:21:55 -0500

    Project initial added

---
+++ Finances/Finances/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finances
+//
+//  Created by DESARROLLO on 21/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Finances/Pods/Target Support Files/Pods-Finances-FinancesUITests/Pods-Finances-FinancesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Finances_FinancesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Finances_FinancesUITests
+@end
